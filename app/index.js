e92c92382953972abee19fd1a33c779a132cb3e9
Anderson Dario <anderson@jp7.com.br>
2016-09-02 20:14:48 -0300

    Leason 2

--- app/index.js
+++ app/index.js
@@ -1,15 +1,26 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js');
 
-var HelloWorld = React.createClass({
-    render: function () {
-        return (
-            <div> Hello World </div>
-        )
+var sentryKey = 'ad3febc85cde41b697bf3216a9f09ec8';
+var sentryApp = '96433';
+var sentryUrl = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+
+var _APP_INFO = {
+    name: 'Github Battle',
+    branch: '4',
+    version: '1.0'
+}
+
+Raven.config(sentryUrl, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch,
     }
-});
+}).install();
 
 ReactDom.render(
-    <HelloWorld />,
+    routes,
     document.getElementById('app')
 );
